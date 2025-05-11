9a3626c51b2d60a37bdafa0b848057294fc55c43
Redddy <midzy0228@gmail.com>
2025-05-11 23:15:10 +0900

    feat: 삐뚤어질테야.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ mod link;
 mod schedule;
 
 use crate::schedule::check_repository_links;
-use std::time::Duration;
+use std::time::Duration; 
 
 #[tokio::main]
 async fn main() {
     let repo_url = "https://github.com/reddevilmidzy/redddy-action";
     let interval_duration = Duration::from_secs(60);
     check_repository_links(repo_url, interval_duration).await;  
-}
+} 
