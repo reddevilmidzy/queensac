6fb60826ecec3c2a95d84af2bb52db545a57a8ea
Redddy <midzy0228@gmail.com>
2025-05-11 23:42:12 +0900

    fix: add blank

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,4 @@ async fn main() {
     let repo_url = "https://github.com/reddevilmidzy/redddy-action";
     let interval_duration = Duration::from_secs(60);
     check_repository_links(repo_url, interval_duration).await;
-}
-
-#[test]
-fn fail_test() {
-    assert!(false);
-}
+} 
