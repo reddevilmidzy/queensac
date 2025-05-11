8073d9436502bb6dd48d8218c3243d752b644208
reddevilmidzy <midzy0228@gmail.com>
2025-05-11 23:28:35 +0900

    feat: add fail test

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ mod link;
 mod schedule;
 
 use crate::schedule::check_repository_links;
-use std::time::Duration; 
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() {
     let repo_url = "https://github.com/reddevilmidzy/redddy-action";
     let interval_duration = Duration::from_secs(60);
-    check_repository_links(repo_url, interval_duration).await;  
-} 
+    check_repository_links(repo_url, interval_duration).await;
+}
+
+#[test]
+fn fail_test() {
+    assert!(false);
+}
